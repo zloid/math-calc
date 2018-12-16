35f28862a2f8430d4f1ad3c06563f205b8e3eda6
zloid <zloid_1@mail.ru>
2018-12-16 15:17:37 +0100

    v0.1.0

--- scripts.js
+++ scripts.js
@@ -1,43 +1,135 @@
-'use strict';
+/*
+*   math-calc v0.1.0
+*   https://github.com/zloid
+*/
 
-;(function () {
-    
-    var firstClick = true;
-    
-    var calcScreen = document.querySelector('#screen');
-    var allGrabButtons = document.querySelectorAll('button');
-    
-    calcScreen.innerHTML = 0;
+;(() => {
 
-    var _loop = function _loop(i) {
-        allGrabButtons[i].addEventListener('click', function () {
+    let screenObj = document.querySelector('#calcScreen'),
+        buttonsArr = document.querySelectorAll('#calcButtonsNum > button,#calcButtonsOperators > button'),
+        evalButtonObj = document.querySelector('#eval'),
+        backspaceButtonObj = document.querySelector('#backspace'),
+        forwardspaceButtonObj = document.querySelector('#forwardspace'),
+        clearAllButtonObj = document.querySelector('#clearAll'),
+        firstClick = false,
+        addCharFromRightBool = true;
 
-            if (allGrabButtons[i].innerHTML == 'Clear') {
-                calcScreen.innerHTML = '';
-            } else if (allGrabButtons[i].innerHTML == ' = ') {
-                
-                let middleResult = eval(calcScreen.innerHTML);
-                
-                    if (isNaN(middleResult)) {
-                        calcScreen.innerHTML = 'NO WAY!';
-                    } else {
-                        calcScreen.innerHTML = middleResult;
-                    }
-                
-                
-            } else {
-                if (calcScreen.innerHTML == 0 && firstClick == true) {
-                    calcScreen.innerHTML = '';
-                    firstClick = false;
-                }
-                
-                calcScreen.innerHTML += allGrabButtons[i].innerHTML;
-            }
-        });
-    };
-
-    for (var i = 0; i < allGrabButtons.length; i++) {
-        _loop(i);
+    //FUNCTIONS
+    function clearAll () {
+        firstClick ? (screenObj.innerHTML = '0') : (screenObj.innerHTML = '');
+        screenObj.style.color = '#fff';
+        firstClick = true;
+        addCharFromRightBool = true;
     }
+
+    function clearErrorFromScreen () {
+        if (screenObj.innerHTML == 'ERROR' || screenObj.innerHTML == 'Infinity') {
+            clearAll();
+            screenObj.style.color = '#fff';
+        }
+    }
+
+    function addCharFromLeft (key) {
+        buttonsArr[key].innerHTML.match(/\d/)
+            ? screenObj.innerHTML = buttonsArr[key].innerHTML + screenObj.innerHTML
+            : screenObj.innerHTML += buttonsArr[key].innerHTML, addCharFromRightBool = true;
+    }
+
+    function addButtonsValueOnScreen (key) {
+        // firstClick || clearAll();
+        clearErrorFromScreen();
+        addCharFromRightBool
+            ? screenObj.innerHTML += buttonsArr[key].innerHTML
+            : addCharFromLeft(key);
+        doRegExpForBadSituations();
+    }
+
+    function doRegExpForBadSituations () {
+        let middleStr;
+        middleStr = screenObj.innerHTML;
+        //begin > 00 > 0
+        middleStr = middleStr.replace(/^0+/, '0');
+        //begin > 02 > 0
+        middleStr = middleStr.replace(/^0(\d|[(])/, '$1');
+        //begin > 012 > 12; + 02 > + 2
+        middleStr = middleStr.replace(/([+-]|÷|\*)\s*0(\d)/, '$1 $2');
+        //++ -- // *** > + - * /
+        middleStr = middleStr.replace(/(\s*\+\s*|\s*-\s*|\s*÷\s*|\s*\*\s*)+/g, ' $1 ');
+        //.... > . > (( > ( > )) > )
+        middleStr = middleStr.replace(/(\.|\(|\))+/g, '$1');
+        //begin > ) > (
+        middleStr = middleStr.replace(/^0\)/, '(');
+        //() > ' '
+        // middleStr = middleStr.replace(/\(\)/, ' ');
+        //431.55.66 + 1.2.3 > 431.5566 + 1.23
+        middleStr = middleStr.replace(/(\d+\.\d+)\./g, '$1');
+        //.7 > 0.7
+        middleStr = middleStr.replace(/[^\d]\./, ' 0.');
+        middleStr = middleStr.replace(/^\./, '0.');
+        //7. + > 7 + 
+        middleStr = middleStr.replace(/([\d])\.\s/g, ' $1 ');
+        // '' * 5 > 5 * 5  ; ÷ 5 > 5 ÷ 5
+        middleStr = middleStr.replace(/^\s*(\*|÷)\s*(\d+)/, '$2 $1 $2 ');
+        screenObj.innerHTML = middleStr;
+    }
+
+    function toEvaluate () {
+        // * 5 > 5 * 5 ; ÷ 5 > 5 ÷ 5
+        screenObj.innerHTML = screenObj.innerHTML.replace(/^\s*(\*|÷)\s*(\d+)/, '$2 $1 $2 ');
+        //5 * > 5 * 5 ; 5 ÷ > 5 ÷ 5
+        screenObj.innerHTML = screenObj.innerHTML.replace(/(\d+)\s*(\*|÷)\s*$/, '$1 $2 $1');
+        // > 0 ; F
+        // screenObj.innerHTML = screenObj.innerHTML.replace(/(\s*\+\s*|\s*-\s*|\s*÷\s*|\s*\*\s*)+/g, ' 0 ');  
+        let middleValue = screenObj.innerHTML.replace(/÷/g, '/');
+        let endValue = eval(middleValue);
+        isNaN(endValue)
+            ? (screenObj.innerHTML = 'ERROR', screenObj.style.color = '#be0000')
+            : screenObj.innerHTML = eval(middleValue);
+    }
+
+    function doBackspace () {
+        clearErrorFromScreen();
+        screenObj.innerHTML = screenObj.innerHTML.trim();
+        screenObj.innerHTML = screenObj.innerHTML.slice(0, -1);
+        screenObj.innerHTML = screenObj.innerHTML.trim();
+        screenObj.innerHTML == '' && (screenObj.innerHTML = '0');
+        addCharFromRightBool = true;
+    }
+
+    function doForwardspace () {
+        clearErrorFromScreen();
+        screenObj.innerHTML = screenObj.innerHTML.trim();
+        screenObj.innerHTML = screenObj.innerHTML.slice(1);
+        screenObj.innerHTML = screenObj.innerHTML.trim();
+        addCharFromRightBool = false;
+        screenObj.innerHTML == '' && (screenObj.innerHTML = '0', addCharFromRightBool = true);
+    }
+
+    //EVENTS
+    //clear
+    clearAllButtonObj.addEventListener('click', () => {
+        firstClick = true;
+        clearAll();
+    })
+    //clicking simple buttons
+    for (let i = 0; i < buttonsArr.length; i++) {
+        buttonsArr[i].addEventListener('click', () => {
+            addButtonsValueOnScreen(i)
+        })
+    }
+    //to evaluate
+    evalButtonObj.addEventListener('click', () => {
+        toEvaluate()
+    });
+    //backspace
+    backspaceButtonObj.addEventListener('click', () => {
+        addCharFromRightBool = true;
+        doBackspace();
+    })
+    //forwardspace
+    forwardspaceButtonObj.addEventListener('click', () => {
+        addCharFromRightBool = false;
+        doForwardspace();
+    })
 
 })();
