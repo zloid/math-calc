5347cfbc8826209926352116ecb9f6578bff4a73
zloid <zloid-1@yandex.by>
2020-12-11 17:07:29 +0100

    add: basic calc buttons + ui tests

--- src/app/App.js
+++ src/app/App.js
@@ -11,8 +11,30 @@ import Reef from 'reefjs'
 const App = (placeToRender = '#root') =>
     new Reef(placeToRender, {
         template: () => /* html */ `<div>
-        <div role="calcMainScreen"></div>
-    </div>`,
+            <div role="calcMainScreen"></div>
+
+            <button role="calcBtn0">0</button>
+            <button role="calcBtn1">1</button>
+            <button role="calcBtn2">2</button>
+            <button role="calcBtn3">3</button>
+            <button role="calcBtn4">4</button>
+            <button role="calcBtn5">5</button>
+            <button role="calcBtn6">6</button>
+            <button role="calcBtn7">7</button>
+            <button role="calcBtn8">8</button>
+            <button role="calcBtn9">9</button>
+
+            <button role="calcBtnPlus">+</button>
+            <button role="calcBtnMinus">-</button>
+            <button role="calcBtnMultiply">*</button>
+            <button role="calcBtnDivision">÷</button>
+
+            <button role="calcBtnClear">C</button>
+            <button role="calcBtnEqual">=</button>
+            <button role="calcBtnPoint">.</button>
+            
+        </div>`,
     })
+
 
 export default App
