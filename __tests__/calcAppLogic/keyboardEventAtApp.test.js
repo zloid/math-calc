d80e3bdb7bd5a07ff49fc6430ddbd59cf66b1a5f
zloid <zloid-1@yandex.by>
2021-01-01 17:59:08 +0100

    fix(test): keyboardEventAtApp, mixed

--- __tests__/calcAppLogic/keyboardEventAtApp.test.js
+++ __tests__/calcAppLogic/keyboardEventAtApp.test.js
@@ -30,6 +30,29 @@ describe('Keyboard and Numpad is working for calc App', () => {
         // calc screen is clear
         expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe('0')
     })
+    it('- 9 - 9 + 1 * 17 / 17 = - 17', () => {
+        // main keyboard
+        fireEvent.keyDown(document, { key: '-', code: 'Minus' })
+        fireEvent.keyDown(document, { key: '9' })
+        // '-' on numPad
+        fireEvent.keyDown(document, { key: '-', code: 'NumpadSubtract' })
+        fireEvent.keyDown(document, { key: '9' })
+        // '+'
+        fireEvent.keyDown(document, { key: '+' })
+        fireEvent.keyDown(document, { key: '1' })
+        // '*'
+        fireEvent.keyDown(document, { key: '*' })
+        fireEvent.keyDown(document, { key: '1' })
+        fireEvent.keyDown(document, { key: '7' })
+        // '/'
+        fireEvent.keyDown(document, { key: '/' })
+        fireEvent.keyDown(document, { key: '1' })
+        fireEvent.keyDown(document, { key: '7' })
+        // equal, numPad key Enter
+        fireEvent.keyDown(document, { key: 'Enter', code: 'NumpadEnter' })
+        // calc screen result
+        expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe('- 17')
+    })
     it('1234567890 + 1234567890 = 2469135780', () => {
         // main keyboard pressing: 123456789
         for (let i = 1; i < 10; i++) {
@@ -59,6 +82,41 @@ describe('Keyboard and Numpad is working for calc App', () => {
         // calc screen result
         expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe(
             '2469135780'
+        )
+    })
+    it('1234567890.1 + 1234567890,1 = 2469135780.2', () => {
+        // main keyboard pressing:
+        for (let i = 1; i < 10; i++) {
+            fireEvent.keyDown(document, {
+                key: `${i}`,
+                code: `Digit${i}`,
+            })
+        }
+        fireEvent.keyDown(document, { key: '0', code: 'Digit0' })
+        // point is work
+        fireEvent.keyDown(document, { key: '.', code: 'Period' })
+        fireEvent.keyDown(document, { key: '1', code: 'Digit1' })
+
+        // Numpad key '+'
+        fireEvent.keyDown(document, { key: '+', code: 'NumpadAdd' })
+
+        // Numpad keyboard pressing:
+        for (let i = 1; i < 10; i++) {
+            fireEvent.keyDown(document, {
+                key: `${i}`,
+                code: `Digit${i}`,
+            })
+        }
+        fireEvent.keyDown(document, { key: '0', code: 'Numpad0' })
+        // comma is work
+        fireEvent.keyDown(document, { key: ',', code: 'NumpadDecimal' })
+        fireEvent.keyDown(document, { key: '1', code: 'Numpad1' })
+
+        // equal, numPad key Enter
+        fireEvent.keyDown(document, { key: 'Enter', code: 'NumpadEnter' })
+        // calc screen result
+        expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe(
+            '2469135780.2'
         )
     })
 })
--- __tests__/calcAppLogic/mixed.test.js
+++ __tests__/calcAppLogic/mixed.test.js
@@ -1,9 +1,17 @@
-import { screen } from '@testing-library/dom'
+import { screen, fireEvent } from '@testing-library/dom'
 import App from '../../src/app/App'
+import store from '../../src/app/store'
+import mapAllDispatch from '../../src/utils/mapAllDispatch'
 
 document.body.innerHTML = /* html */ '<div role="mainId" id="root"></div>'
 
+!globalThis.mapAllDispatch && (globalThis.mapAllDispatch = mapAllDispatch)
+
 App('#root').render()
+// render each time when dispatch redux actions
+store.subscribe(() => {
+    App('#root').render()
+})
 
 describe('div#root', () => {
     it('should exist', () => {
@@ -11,8 +19,23 @@ describe('div#root', () => {
     })
 })
 
-describe('App', () => {
+describe('App initial screen value', () => {
     it('calc [screen] value should be 0', () => {
+        expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe('0')
+    })
+})
+
+describe('App clear button', () => {
+    beforeEach(() => {
+        //initial, must be here
+        document.getElementById('root').innerHTML += ''
+    })
+    it('click C(clear) button for clear calc screen', () => {
+        fireEvent.click(screen.getByRole(/calcBtn9/i))
+        // calc screen not clear
+        expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe('9')
+        // clear screen
+        fireEvent.click(screen.getByRole(/calcBtnClear/i))
         expect(screen.getByRole(/calcMainScreen/i).textContent.trim()).toBe('0')
     })
 })
